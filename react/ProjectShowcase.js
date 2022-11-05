856876da02a152bf46b551c56e38d595f8fe26d7
jackdawkins11 <47468397+jackdawkins11@users.noreply.github.com>
2022-11-05 06:56:04 -0500

    Update ProjectShowcase.js

--- react/ProjectShowcase.js
+++ react/ProjectShowcase.js
@@ -53,8 +53,8 @@ function ProjectShowcase(props) {
                 <img style={logoStyle} src="./img/nvidia-cuda-logo.jpg"></img>
             </div>
         </div>
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: chessWidth }}>
-            <img src="./img/image.jpeg"></img>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <img style={width: chessWeidth} src="./img/image.jpeg"></img>
         </div>
     </div>
 }
