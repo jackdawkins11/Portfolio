1d15f5a70712541a5940690e6e6bb11a38945494
root <root@ip-172-31-19-10.ec2.internal>
2022-11-16 14:35:35 +0000

    Moved entire simulation into BackgroundCanvas

--- react/App.js
+++ react/App.js
@@ -4,7 +4,6 @@ import { render } from "react-dom";
 import { BackgroundCanvas } from "./BackgroundCanvas";
 import { HomePage } from './HomePage';
 import { LandingPage } from './LandingPage';
-import { Simulation } from './Simulation';
 
 /*
     This is the root component.
@@ -17,43 +16,16 @@ class App extends React.Component {
     constructor(props){
         super(props)
 
-        //The simulation being run to display the background is a state variable
-        let simul = new Simulation(10000, 10000)
-        simul.randomAsteroids()
-
-        //state also stores whether to display landing page or home page
         this.state = {
-            simul: simul,
-            landingPage: true
+            landingPage: true,
+	    simulPaused: false
         }
 
     }
 
-    componentDidMount(){
-        //Start the animation loop
-        requestAnimationFrame(this.updateBackgroundSimulation.bind(this))
-    }
-
-    //Updates the simulation
-    updateBackgroundSimulation(){
-        //Update state of simulation
-        this.state.simul.update()
-
-        //Let react know state was updated
-        this.setState({})
-
-        //Animation loop
-        requestAnimationFrame(this.updateBackgroundSimulation.bind(this))
-    }
-
-
     toggleSimulationPaused(){
-
-        //Pause the simulation
-        this.state.simul.paused = !this.state.simul.paused
-
         //Let react know state was updated
-        this.setState({})
+        this.setState({simulPaused: !this.state.simulPaused})
     }
 
     //Change state to display home page
@@ -71,12 +43,12 @@ class App extends React.Component {
         }else{
             content = <HomePage
                         toggleSimulationPaused={this.toggleSimulationPaused.bind(this)}
-                        simulNotPaused={!this.state.simul.paused} />
+                        simulNotPaused={!this.state.simulPaused} />
         }
 
         //Render the background simulation and content
-        return <div style={{fontFamily: "sans-serif"}}>
-            <BackgroundCanvas simul={this.state.simul} />
+        return <div style={{fontFamily: "sans-serif"}}  >
+            <BackgroundCanvas paused={this.state.simulPaused}/>
             {content}
         </div>
     }
--- react/BackgroundCanvas.js
+++ react/BackgroundCanvas.js
@@ -1,31 +1,164 @@
 import React from 'react'
 
-/*
-    Renders a canvas. When props.simul is updated,
-    the canvas is repainted using props.simul
-*/
+//An asteroid stores a location,
+//a velocity, and details about drawing it
+class Asteroid {
+    //Initialize a new asteroid
+    constructor(x, y, vx, vy, MAXRADIUS) {
+        this.x = x
+        this.y = y
+        this.vx = vx
+        this.vy = vy
+        this.r = Math.random() * MAXRADIUS
+
+        var rand = Math.random()
+        if (rand <= 0.5) {
+            this.fillStyle = "rgba(255, 255, 255, 1)"
+            this.shadowColor = "rgba(255, 255, 255, 0.5)"
+            this.shadowBlur = 3
+        }
+        else if (rand > 0.75) {
+            this.fillStyle = "rgba(255, 254, 196, 1)"
+            this.shadowColor = "rgba(255, 254, 196, 0.5)"
+            this.shadowBlur = 4
+        }
+        else {
+            this.fillStyle = "rgba(192, 247, 255, 1)"
+            this.shadowColor = "rgba(192, 247, 255, 0.5)"
+            this.shadowBlur = 7
+        }
+    }
+
+    //Move it as if one unit of time has passed
+    update() {
+        this.x += this.vx
+        this.y += this.vy
+    }
+
+    //Check if the asteroid has left the screen
+    inBounds(w, h) {
+        return this.x < w && this.y < h
+    }
+
+    //Draw the asteroid to the canvas
+    draw(canvas, ctx, simulWidth, simulHeight) {
+
+	let px = (this.x / simulWidth) * canvas.width * 1.05
+	let py = (this.y / simulHeight) * canvas.height * 1.05
+
+        ctx.beginPath()
+        ctx.arc(px, py, this.r, 0, 2 * Math.PI, false)
+        ctx.fillStyle = this.fillStyle
+        ctx.shadowColor = this.shadowColor
+        ctx.shadowBlur = this.shadowBlur
+        ctx.fill()
+    }
+}
+class Simulation {
+    //This stores the state of the simulation
+    constructor(width, height) {
+        //
+        this.asteroids = []
+        this.width = width
+        this.height = height
+        this.paused = false
+    
+	this.MAXVELOCITY = 5
+	this.PROBNEWASTEROID = 0.1
+	this.MAXRADIUS = 15
+
+    }
+
+    //Draw the current state of the simulation to the screen
+    paint(canvas, ctx) {
+        //clear the screen
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        //draw each asteroid
+        for (let asteroid of this.asteroids) {
+            asteroid.draw(canvas, ctx, this.width, this.height)
+        }
+    }
+
+    //Calls update on each asteroid
+    //Deletes all out of bounds asteroids
+    //Possibly creates a new asteroid
+    update() {
+        if( this.paused ){
+            return
+        }
+        for (let asteroid of this.asteroids) {
+            asteroid.update()
+        }
+        this.asteroids = this.asteroids.filter((asteroid) => {
+            return asteroid.inBounds(this.width, this.height)
+        })
+        this.randomAsteroids()
+    }
+
+    //Possibly adds a new asteroid
+    randomAsteroids() {
+        if (Math.random() <= this.PROBNEWASTEROID) {
+            this.asteroids.push(
+                new Asteroid(
+                    Math.random() * this.width,         //random x
+                    0,                                  //top of screen
+                    ( Math.random() * 0.5 - 0.25 ) * this.MAXVELOCITY,  //vx in (-0.25 , 0.25)
+                    Math.random() * this.MAXVELOCITY,  //vy in (0, 1)
+		    this.MAXRADIUS
+		)
+            )
+        }
+    }
+}
+
+
 class BackgroundCanvas extends React.Component {
     constructor(props){
         super(props)
-
-        //We need access to the canvas
+        this.simul = new Simulation(1000, 1000)
+        this.simul.randomAsteroids()
         this.canvasRef = React.createRef()
     }
 
-    componentDidUpdate(){
-        //Get the dom node
+    componentDidMount(){
+	//window.addEventListener( 'keydown', this.onKeyDown.bind(this) )
+        //Start the animation loop
+        requestAnimationFrame(this.updateBackgroundSimulation.bind(this))
+    }
+    
+    //Updates the simulation
+    updateBackgroundSimulation(){
+        this.simul.update()
         this.canvas = this.canvasRef.current
-
-        //Set its size to the size of the window
         this.canvas.width = window.innerWidth
         this.canvas.height = window.innerHeight
+        this.simul.paint(this.canvas, this.canvas.getContext('2d'))
+        requestAnimationFrame(this.updateBackgroundSimulation.bind(this))
+    }
 
-        //Repaint the simulation
-        this.props.simul.paint(this.canvas, this.canvas.getContext('2d'))
+    onKeyDown(e){
+    	if( e.keyCode == 37 ){			//left
+		this.simul.MAXVELOCITY /= 2
+	}else if( e.keyCode == 38 ){		//up
+		this.simul.PROBNEWASTEROID += 0.1
+		if( this.simul.PROBNEWASTEROID > 1 ){
+			this.simul.PROBNEWASTEROID = 1
+		}
+	}else if( e.keyCode == 39 ){		//right
+		this.simul.MAXVELOCITY *= 2
+	}else if( e.keyCode == 40 ){		//down
+		this.simul.PROBNEWASTEROID -= 0.1
+		if( this.simul.PROBNEWASTEROID < 0 ){
+			this.simul.PROBNEWASTEROID = 0
+		}
+	}else if( e.keyCode == 32 ){		//enter
+        	this.simul.paused = !this.simul.paused
+	}
     }
 
     render(){
-        //Render the canvas
+	 this.simul.paused = this.props.paused
+	//Render the background simulation
         return <canvas ref={this.canvasRef}
             style={{
                 backgroundColor: "#2e2e2e",
